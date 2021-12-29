3d38c49791c3bdd92f39065a76870e23ee01d062
com <x7788@naver.com>
2021-12-29 18:08:13 +0900

    접기 / 펼치기 작업

--- js/app.js
+++ js/app.js
@@ -22,3 +22,26 @@ $('.lightbox').lightGallery({
   autoplay: true,
   progressBar: true,
 })
+
+// 접기, 펼치기
+$('.btn').click(function (e) {
+  e.preventDefault()
+  $('.nav').slideToggle()
+  $('.btn').toggleClass('open')
+
+  if ($('.btn').hasClass('open')) {
+    // open이 있을 때, attr 속성 변경
+    $('.btn').find('i').attr('class', 'fa fa-angle-up')
+  } else {
+    // open이 없을 때
+    $('.btn').find('i').attr('class', 'fa fa-angle-down')
+  }
+})
+
+$(window).resize(function () {
+  const wWidth = $(window).width()
+
+  if (wWidth > 600) {
+    $('.nav').removeAttr('style')
+  }
+})
